a1422186244c500cf8df1559706c34b9652ad325
Nuno Nogueira <nunofmn@gmail.com>
2015-05-12 14:48:15 +0100

    Initial force directed graph with dummy data.

--- server.py
+++ server.py
@@ -6,6 +6,30 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route("/entities")
+def entities():
+    return render_template('entities.html')
+
+@app.route("/entities/data")
+def entitiesdata():
+    return jsonify(
+        {
+          "nodes":[
+              {"name":"Pedro Passos-Coelho"},
+              {"name":"Paulo Portas"},
+              {"name":"Estradas de Portugal"},
+              {"name":"EDP"}
+            ],
+          "links":[
+              {"source":0,"target":1,"value":5},
+              {"source":0,"target":3,"value":2},
+              {"source":1,"target":3,"value":2},
+              {"source":1,"target":2,"value":1},
+              {"source":2,"target":2,"value":4}
+            ]
+        }
+    );
+
 @app.route("/search", methods=['POST'])
 def search():
 
